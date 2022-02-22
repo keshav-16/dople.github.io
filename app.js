1b1110dd326015fc7ce12a909bd911e132eede6f
keshav-16 <keshav162001@gmail.com>
2022-02-22 19:05:58 +0530

    app.js sepretely

---
+++ app.js
@@ -0,0 +1,12 @@
+var navLinks = document.getElementById("navLinks");
+
+        function showMenu(){
+            navLinks.style.right = "0px";
+            navLinks.style.display = "block";
+        }
+
+        function hideMenu(){
+            navLinks.style.right = "-200px";
+            navLinks.style.display = "none";
+        }
+    
